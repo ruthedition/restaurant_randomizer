93b53a7f0fc15598aa7e720ad1c9363019c9cf73
Rkebede <rutht.kebede@gmail.com>
2020-08-02 15:57:03 -0500

    adds validators for models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
-  # validates :name, :presence true 
-  # validates :name, :uniqueness true 
+  validate :name, presence: true 
+  validate :name, uniqueness: true 
   has_many :restaurant_food_types, dependent: :destroy
   has_many :food_types, through: :restaurant_food_types
 end
