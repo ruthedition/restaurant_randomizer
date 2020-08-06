68a817d3d2763385800644879baecac070f47a96
Rkebede <rutht.kebede@gmail.com>
2020-08-06 17:49:47 -0500

    creates UsersControllers actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,17 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end 
+
+  def create
+    user = User.create(user_params)
+    params[:user][:password_digest]
+    session[:user_id] = @user.id
+    redirect_to user_path
+  end 
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end  
+
 end
 
