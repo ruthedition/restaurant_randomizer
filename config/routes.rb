6fe95fb852a875c9480b3bb9e451c4e5a90c53ff
Rkebede <rutht.kebede@gmail.com>
2020-08-06 17:36:25 -0500

    creates users table, controller, and new form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/login', to: "sessions#new", as: "login"
   resources :restaurants
-
+  resources :users, only: [:index, :new]
   resources :food_types, only: [:index] do 
     resources :restaurants, only: [:index, :new], controller: 'food_types/restaurants'
   end
 
 end
+
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_033033) do
+ActiveRecord::Schema.define(version: 2020_08_06_210444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 2020_08_02_033033) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
   end
 
   add_foreign_key "restaurant_food_types", "food_types"
